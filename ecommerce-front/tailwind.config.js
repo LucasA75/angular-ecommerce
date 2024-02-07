74fa1b209265c8fa06c765e917a0cc7dd324d46f
LucasA75 <lucasacevedocastro@gmail.com>
2024-02-07 14:40:00 -0300

    chore: add dependencies(tailwind and icons)

---
+++ ecommerce-front/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customRed: {
+          500: '#f6f6d3', 
+          700: '#cacaab',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
