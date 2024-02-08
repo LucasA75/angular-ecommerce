d1839bf72097940a45a46f168461a5cf26f4c7ae
Anexxs <hector.espinosa.vega@gmail.com>
2024-02-07 22:29:37 -0300

    1st try navbar

--- ecommerce-front/src/app/app.component.ts
+++ ecommerce-front/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ItemComponent } from './components/item/item.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ItemComponent],
+  imports: [RouterOutlet,ItemComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
