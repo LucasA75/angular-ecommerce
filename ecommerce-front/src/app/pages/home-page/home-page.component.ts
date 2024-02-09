7d6632f14e17516742b59ef0e420ada718fbeeec
LucasA75 <lucasacevedocastro@gmail.com>
2024-02-09 15:07:07 -0300

    feat: add v1 cart product

--- ecommerce-front/src/app/pages/home-page/home-page.component.ts
+++ ecommerce-front/src/app/pages/home-page/home-page.component.ts
@@ -4,11 +4,12 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faGift } from '@fortawesome/free-solid-svg-icons';
+import { CardProductComponent } from '../../components/card-product/card-product.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [IconContainerComponent,FontAwesomeModule],
+  imports: [IconContainerComponent,FontAwesomeModule,CardProductComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
