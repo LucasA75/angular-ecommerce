58403163f029c6194e289d53160ffd2c4435d832
LucasA75 <lucasacevedocastro@gmail.com>
2024-02-14 23:54:46 -0300

    chore: change localhost

--- ecommerce-back/src/app.module.ts
+++ ecommerce-back/src/app.module.ts
@@ -8,7 +8,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/tutorial'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/tutorial'),
     ProductsModule,
      ClientsModule,
       OrdersModule],
