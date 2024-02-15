6dec8dfdf386ff4ec494855cc30e97934c9cb31c
LucasA75 <lucasacevedocastro@gmail.com>
2024-02-14 23:36:12 -0300

    feat: create product and client

--- ecommerce-back/src/app.module.ts
+++ ecommerce-back/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { ClientsModule } from './clients/clients.module';
+import { OrdersModule } from './orders/orders.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/tutorial'),
+    ProductsModule,
+     ClientsModule,
+      OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
