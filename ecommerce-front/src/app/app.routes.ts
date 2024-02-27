cd07f6f5ff08da4c6a953ab39fee13595cdea8e1
LucasA75 <lucasacevedocastro@gmail.com>
2024-02-27 16:10:06 -0300

    feat: add registrer and login pages

--- ecommerce-front/src/app/app.routes.ts
+++ ecommerce-front/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { DetailProductPageComponent } from './pages/detail-product-page/detail-product-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegistrerPageComponent } from './pages/registrer-page/registrer-page.component';
 
 export const routes: Routes = [
 
     { path: '', component: HomePageComponent },
     {path: "product/:id", component: DetailProductPageComponent},
+    {path:"login", component: LoginPageComponent},
+    {path:"registrer", component:RegistrerPageComponent},
     { path: '**', component: PageNotFoundComponent },
 ];
