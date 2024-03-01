613710d933530fb6f364aa5b8d127652b9434cc0
Anexxs <hector.espinosa.vega@gmail.com>
2024-02-29 22:09:08 -0300

    feat: navbar 29/2

--- ecommerce-front/src/app/components/navbar/navbar.component.ts
+++ ecommerce-front/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  amugus=faBars
+  holamundo(){
+    console.log("hola soy hector")
+  }
 }
