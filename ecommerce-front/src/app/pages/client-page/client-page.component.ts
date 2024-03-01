313ff471cb7ab5de52b59ba64a0c0e5e3f9fe1c2
LucasA75 <lucasacevedocastro@gmail.com>
2024-03-01 00:52:04 -0300

    refactor: add sideBar

--- ecommerce-front/src/app/pages/client-page/client-page.component.ts
+++ ecommerce-front/src/app/pages/client-page/client-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from '../../components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-client-page',
   standalone: true,
-  imports: [],
+  imports: [SideBarComponent],
   templateUrl: './client-page.component.html',
   styleUrl: './client-page.component.css'
 })
