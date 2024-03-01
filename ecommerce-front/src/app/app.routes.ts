9a272de84be0ce26734da051e2bbe49cc1cbd474
LucasA75 <lucasacevedocastro@gmail.com>
2024-02-29 21:40:39 -0300

    feat: add initial page user

--- ecommerce-front/src/app/app.routes.ts
+++ ecommerce-front/src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
 import { DetailProductPageComponent } from './pages/detail-product-page/detail-product-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegistrerPageComponent } from './pages/registrer-page/registrer-page.component';
+import { ClientPageComponent } from './pages/client-page/client-page.component';
 
 export const routes: Routes = [
-
-    { path: '', component: HomePageComponent },
-    {path: "product/:id", component: DetailProductPageComponent},
-    {path:"login", component: LoginPageComponent},
-    {path:"registrer", component:RegistrerPageComponent},
-    { path: '**', component: PageNotFoundComponent },
+  { path: '', component: HomePageComponent },
+  { path: 'product/:id', component: DetailProductPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'registrer', component: RegistrerPageComponent },
+  { path: 'user', component: ClientPageComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
